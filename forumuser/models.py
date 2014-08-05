79bb94f51cd2dca65479cb39f6c365c4c372b0ca
Brian Wilson <bwilson@twig-world.com>
2014-08-05 19:49:28 +0100

    Add items per page as a preference to the forumm user model

--- forumuser/models.py
+++ forumuser/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 
 class ForumUser(AbstractUser):
+    items_per_page = models.PositiveSmallIntegerField(blank=True, null=True)
+
     def __unicode__(self):
         return '%(username)s (%(email)s)' % {
             'username': self.username,
