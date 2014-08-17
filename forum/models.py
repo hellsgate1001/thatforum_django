1fd8637e53e142e2fbefbd6393c9a8c746db13f6
Brian Wilson <brianwilson1001@hotmail.com>
2014-08-17 13:24:35 +0100

    Initial models and migrations for forum app

---
+++ forum/models.py
@@ -0,0 +1,37 @@
+from django.conf import settings
+from django.db import models
+
+
+class ForumCategory(models.Model):
+    parent = models.ForeignKey('self', blank=True, null=True)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    order = models.PositiveIntegerField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class ForumThread(models.Model):
+    category = models.ForeignKey(ForumCategory)
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
+
+
+class ForumPost(models.Model):
+    thread = models.ForeignKey(ForumThread)
+    post = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    reply_to = models.ForeignKey('self', blank=True, null=True)
+
+    def __unicode__(self):
+        return '%(thread)s - %(pk)s' % {
+            'thread': self.thread.title,
+            'pk': self.pk
+        }
