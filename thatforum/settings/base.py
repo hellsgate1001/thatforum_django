1c2fa5868001cffdf4b0bee882e72b9bb96b30ab
Brian Wilson <brianwilson1001@hotmail.com>
2015-04-06 14:20:33 +0000

    Move settings to directory Move main project templates to thatforum directory Add static files

--- thatforum/settings/base.py
+++ thatforum/settings/base.py
@@ -12,12 +12,23 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# Normally you should not import ANYTHING from Django directly
+# into your settings, but ImproperlyConfigured is an exception.
+from django.core.exceptions import ImproperlyConfigured
+def get_env_variable(var_name):
+    """ Get the environment variable or return exception """
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qrifvyy5@s##u^6d+zz6r=&g(4r)j2dj^m2%-*)r1a&*bd66j^'
+SECRET_KEY = 'ytExyNIy87Ivrn59kwHvwrd4XAc5ZlZhJwOgN-It'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -67,19 +78,19 @@ WSGI_APPLICATION = 'thatforum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
@@ -92,6 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/projects/thatforum_django/static/'
 
 AUTH_USER_MODEL = 'forumuser.ForumUser'
 
